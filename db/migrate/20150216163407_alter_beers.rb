09ea647dcfa003d3fb1ad95d379c3d6fc0d06728
Aku Silvennoinen <akusilvennoinen@dsl-hkibrasgw3-54fb71-199.dhcp.inet.fi>
2015-02-22 12:55:28 +0200

    w 6 u 9

--- db/migrate/20150216163407_alter_beers.rb
+++ db/migrate/20150216163407_alter_beers.rb
@@ -1,16 +1,16 @@
 class AlterBeers < ActiveRecord::Migration
   def change
     t=[]
-    Beer.all.each { |b| if not t.include?(b.style) then t << b.style end }
+    Beer.all.each { |b| if not t.include?(Beer.find_by_sql("select style from beers where id = #{b.id}")[0]["style"]) then t << Beer.find_by_sql("select style from beers where id = #{b.id}")[0]["style"] end }
     t.each { |style| Style.create name:style, description:style }
     
     change_table :beers do |t|
       t.rename :style, :old_style
       t.integer :style_id
     end
-
+    byebug
     Beer.all.each do |b|
-      b.update("style_id", Style.find_by(name:b.old_style).id) 
+      b.update_attribute("style_id", Style.find_by(name: b.old_style).id) 
     end
     
     change_table :beers do |t|
